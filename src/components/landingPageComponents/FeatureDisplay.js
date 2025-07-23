a1373ab1ed2c4821b87e02f75c56cf9aad0fea9b
Daniyal Master <daniyalmaster693@gmail.com>
2025-07-23 10:02:32 -0400

    Initial Commit

--- src/components/landingPageComponents/FeatureDisplay.js
+++ src/components/landingPageComponents/FeatureDisplay.js
@@ -1,37 +1,61 @@
-import { Sparkle, } from 'lucide-react';
+import { Sparkle } from "lucide-react";
+
+const features = [
+  {
+    title: "Additional Zones",
+    description:
+      "Trigger actions when moving your mouse the middle of any screen edge.",
+    imgSrc: "https://dropoverapp.com/assets/images/tile-power-users.jpg",
+  },
+  {
+    title: "Launch Apps",
+    description: "Launch apps directly from corners and zones.",
+    imgSrc: "https://dropoverapp.com/assets/images/tile-upload.jpg",
+  },
+  {
+    title: "Run Shortcuts",
+    description:
+      "Trigger actions when moving your mouse the middle of any screen edge.",
+    imgSrc: "https://dropoverapp.com/assets/images/settings.jpg",
+  },
+  {
+    title: "Access System Commands",
+    description: "Launch apps directly from corners and zones.",
+    imgSrc: "https://dropoverapp.com/assets/images/tile-instant-actions.jpg",
+  },
+];
 
 function FeatureDisplay() {
-    return (
-        <section className="bg-slate-50 py-24 pb-16">
-            <div className='max-w-sm sm:max-w-2xl lg:max-w-3xl mx-auto flex flex-col gap-4'>
-                <h2 className='tracking-tight font-bold text-center md:text-left text-3xl lg:text-5xl lg:leading-[3.5rem]'>
-                    All you need to do Lorem ipsum dolor sit amet consectetur
-                </h2>
-                <p className='font-semibold my-4 text-center md:text-left text-gray-700'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In minima incidunt, rerum quia asperiores fugit ipsam. Labore iusto dolor autem. Lorem ipsum dolor sit amet consectetur adipisicing el
-                </p>
+  return (
+    <section className="bg-[#FFF] py-24 pb-16" id="features">
+      <div className="max-w-sm sm:max-w-4xl lg:max-w-6xl mx-auto flex flex-col gap-20">
+        <h2 className="tracking-tight font-bold text-center text-3xl lg:text-5xl lg:leading-[3.5rem]">
+          More than just corners.
+        </h2>
 
-                <div className='flex flex-wrap md:flex-nowrap items-center justify-center md:justify-between gap-8 md:gap-0 mt-4'>
-                    <div className='flex flex-col items-center gap-2 group cursor-pointer'>
-                        <Sparkle className={`h-5 w-5 md:h-10 md:w-10group-hover:text-gray-600 transition-colors duration-200`} />
-                        <p className={`text-sm font-semibold  group-hover:text-gray-600 transition-colors duration-200`}>Feature One</p>
-                    </div>
-                    <div className='flex flex-col items-center gap-2 group cursor-pointer'>
-                        <Sparkle className={`h-5 w-5 md:h-10 md:w-10group-hover:text-gray-600 transition-colors duration-200`} />
-                        <p className={`text-sm font-semibold  group-hover:text-gray-600 transition-colors duration-200`}>Feature Two</p>
-                    </div>
-                    <div className='flex flex-col items-center gap-2 group cursor-pointer'>
-                        <Sparkle className={`h-5 w-5 md:h-10 md:w-10group-hover:text-gray-600 transition-colors duration-200`} />
-                        <p className={`text-sm font-semibold  group-hover:text-gray-600 transition-colors duration-200`}>Feature Three</p>
-                    </div>
-                    <div className='flex flex-col items-center gap-2 group cursor-pointer'>
-                        <Sparkle className={`h-5 w-5 md:h-10 md:w-10group-hover:text-gray-600 transition-colors duration-200`} />
-                        <p className={`text-sm font-semibold  group-hover:text-gray-600 transition-colors duration-200`}>Feature Four</p>
-                    </div>
-                </div>
+        {features.map((feature, i) => (
+          <div
+            key={feature.title}
+            className={`flex items-center gap-24 ${
+              i % 2 === 1 ? "flex-row-reverse" : "flex-row"
+            }`}
+          >
+            <img
+              src={feature.imgSrc}
+              alt={feature.title}
+              className="w-[460px] h-[320px] rounded-[2rem] flex-shrink-0"
+            />
+            <div className="flex-1 max-w-2xl">
+              <h3 className="text-3xl font-semibold mb-3">{feature.title}</h3>
+              <p className="text-gray-700 break-words leading-relaxed text-med">
+                {feature.description}
+              </p>
             </div>
-        </section>
-    )
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default FeatureDisplay
+export default FeatureDisplay;
