1a75bcd39b4169c78fd9118e8c7ee8516caa3439
Daniyal Master <daniyalmaster693@gmail.com>
2025-07-23 11:57:14 -0400

    Updated footer

--- src/components/landingPageComponents/Footer.js
+++ src/components/landingPageComponents/Footer.js
@@ -4,21 +4,19 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="bg-slate-50">
-      <MaxWidthWrapper className="py-14 pb-20 flex flex-col items-center justify-center md:items-start md:justify-between md:flex-row">
-        <div className="max-w-[16rem] flex flex-col space-y-2 items-center justify-center md:items-start md:justify-normal">
+      <MaxWidthWrapper className="py-14 pb-10 flex flex-col items-center justify-center md:items-start md:justify-between md:flex-row">
+        <div className="max-w-[16rem] flex flex-col space-y-1 items-center justify-center md:items-start md:justify-normal">
           <Link href="/" className="flex items-center z-40 font-bold text-lg">
             SuperCorners
           </Link>
-
-          <p className="text-gray-700 md:text-[0.875rem] font-medium text-center md:text-left">
-            Supercharge your Mac's Corners
+          <p className="text-gray-700 md:text-[0.875rem] font-medium text-center md:text-left whitespace-nowrap">
+            © {new Date().getFullYear()} Daniyal Master. All rights reserved.
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-10 md:gap-24 mt-10 md:mt-0">
-          <div className="flex flex-col items-center md:items-start px-4">
-            <h3 className="font-semibold text-gray-800 mb-2">Links</h3>
-            <ul className="space-y-2 text-gray-600 text-sm text-center md:text-left">
+        <div className="flex flex-row gap-10 mt-20 md:mt-0">
+          <div className="flex items-center md:items-start px-8 py-4">
+            <ul className="flex flex-row space-x-4 text-gray-600 text-sm text-center md:text-left">
               <li>
                 <Link
                   href="#features"
@@ -35,20 +33,6 @@ function Footer() {
                   FAQ
                 </Link>
               </li>
-              <li>
-                <Link
-                  href="#download"
-                  className="text-sm font-normal text-gray-700 hover:underline hover:underline-offset-1"
-                >
-                  Download
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex flex-col items-center md:items-start px-4">
-            <h3 className="font-semibold text-gray-800 mb-2">Legal</h3>
-            <ul className="space-y-2 text-gray-600 text-sm text-center md:text-left">
               <li className="hover:underline hover:underline-offset-1">
                 <Link href="/">Privacy Policy</Link>
               </li>
